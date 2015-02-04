bddfbf0f15f2777256e3ac739e96925b55278622
David Raleigh <davidraleigh@gmail.com>
2015-02-04 14:03:32 -0800

    Week 1 Percolation not implemented

---
+++ src/davidraleigh/PercolationStats.java
@@ -0,0 +1,33 @@
+package davidraleigh;
+
+/**
+ * Created by davidraleigh on 2/3/15.
+ */
+public class PercolationStats {
+
+    public PercolationStats(int N, int T)
+    {
+
+    }// perform T independent experiments on an N-by-N grid
+
+    public double mean()
+    {
+        return Double.NaN;
+    }// sample mean of percolation threshold
+
+    public double stddev()
+    {
+        return Double.NaN;
+    }// sample standard deviation of percolation threshold
+
+    public double confidenceLo()
+    {
+        return Double.NaN;
+    }// low  endpoint of 95% confidence interval
+
+    public double confidenceHi()
+    {
+        return Double.NaN;
+    }// high endpoint of 95% confidence interval
+
+}
---
+++ tests/davidraleigh/PercolationStatsTest.java
@@ -0,0 +1,40 @@
+package davidraleigh;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PercolationStatsTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testMean() throws Exception {
+
+    }
+
+    @Test
+    public void testStddev() throws Exception {
+
+    }
+
+    @Test
+    public void testConfidenceLo() throws Exception {
+
+    }
+
+    @Test
+    public void testConfidenceHi() throws Exception {
+
+    }
+}
